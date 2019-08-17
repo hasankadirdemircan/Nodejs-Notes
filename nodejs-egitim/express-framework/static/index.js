e32879274d0a90fec281543c74f153027b489a75
hkdemircan <hkdemircan06@gmail.com>
2019-08-18 01:56:19 +0300

    static public directory example added.

--- nodejs-egitim/express-framework/static/index.js
+++ nodejs-egitim/express-framework/static/index.js
@@ -1,23 +1,11 @@
-//npm install express --save
-//npm install pug --save
-
 const express = require('express');
 const app = express();
 
 app.set('view engine', 'pug');
-//anasayfaya girdiginde.
+app.use(express.static('public'))
 app.get('/', (req, res)=>{
-    res.render('index.pug', {name:'kadir', surname:'demircan'} );
+    res.render('index.pug');
 });
-
-app.get('/home', (req, res)=>{
-    res.render('home.pug');
-});
-
-app.get('/contact', (req, res)=>{
-    res.render('contact.pug');
-});
-
 
 app.listen((1234), () => {
     console.log('App listening on port 3002!');
