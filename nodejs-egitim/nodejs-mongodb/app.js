6e16c56275cc4ccc4ccc9e993b0e13921f7e6d0f
hkdemircan <hkdemircan06@gmail.com>
2019-08-22 20:44:33 +0300

    mongodb bağlantısı tamamlandı.

--- nodejs-egitim/nodejs-mongodb/app.js
+++ nodejs-egitim/nodejs-mongodb/app.js
@@ -4,11 +4,22 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//mongoose dahil ediyoruz.
+const mongoose = require('mongoose');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+//mongoose connection ayarini yapiyoruz.
+mongoose.connect('mongodb://localhost/testdb', { useNewUrlParser: true });
+mongoose.connection.on('open', ()=>{
+  console.log('mongodb connection succesfull');
+});
+mongoose.connection.on('error', (err)=>{
+  console.log('mongodb connection failed.', err);
+});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
